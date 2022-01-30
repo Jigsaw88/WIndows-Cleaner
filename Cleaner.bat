1eccacc84455721e76079b3f3669801cce4efa07
Jigsaw <71355908+Jigsaw88@users.noreply.github.com>
2022-01-30 00:51:51 +0000

    Typo fix

--- Cleaner.bat
+++ Cleaner.bat
@@ -4,6 +4,6 @@ del /s /f /q %userprofile%\Recent\*.*
 del /s /f /q C:\Windows\Prefetch\*.*
 del /s /f /q C:\Windows\Temp\*.*
 del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
-del /s /f /1 C:\Windows\SoftwareDistribution\*.*
+del /s /f /q C:\Windows\SoftwareDistribution\*.*
 echo Cleaned.
 pause
